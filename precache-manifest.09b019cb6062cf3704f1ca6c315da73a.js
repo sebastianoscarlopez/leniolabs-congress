6385c33d070151c5b11613bd5f30d41c39e8de6c
sebas <sebastianoscarlopz@gmail.com>
2019-11-09 11:58:35 -0300

    Updates

---
+++ precache-manifest.09b019cb6062cf3704f1ca6c315da73a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d16e91aa7fe2c1981a6711371cd35e9",
+    "url": "/leniolabs-congress/index.html"
+  },
+  {
+    "revision": "b716fcb444bf2e153a51",
+    "url": "/leniolabs-congress/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a298ec9fd0a558945341",
+    "url": "/leniolabs-congress/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b716fcb444bf2e153a51",
+    "url": "/leniolabs-congress/static/js/2.7ec15d5f.chunk.js"
+  },
+  {
+    "revision": "a298ec9fd0a558945341",
+    "url": "/leniolabs-congress/static/js/main.1827d3e1.chunk.js"
+  },
+  {
+    "revision": "bf70b4975526e9d1c7bc",
+    "url": "/leniolabs-congress/static/js/runtime-main.77ac91a3.js"
+  },
+  {
+    "revision": "1d6348f9598f3a3a519743decf541a58",
+    "url": "/leniolabs-congress/static/media/leniolabs-isologo.1d6348f9.svg"
+  }
+]);
